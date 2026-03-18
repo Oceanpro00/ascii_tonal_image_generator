a0390572b5fe772dc43b02bb959a1af0124299c3
sean.schallberger@gmail.com <sean.schallberger@gmail.com>
2026-03-18 13:35:21 -0400

    Starting v2 and outlined prime targets

---
+++ scripts/ascii_tonal_generator_v2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 18 13:13:02 2026
+
+Project: ascii_tonal_generator
+
+Prime Targets:
+    - Clean up v1 Function logic
+        - Split each logi part of the function into seperate functions
+        - Have 1 function call all other functions thus giving us cleaner code
+    - Add other color ways
+
+@author: seanschallberger
+"""
+
+# =============================================================================
+# Function 1:
+# =============================================================================
