b1621300dbdac0c1400f9eebcea49af199020977
sean.schallberger@gmail.com <sean.schallberger@gmail.com>
2026-03-21 10:54:50 -0400

    Fully Functional minimal v2: Takes Ascii.txt --> Picture.ppm

--- scripts/ascii_tonal_generator_v2.py
+++ scripts/ascii_tonal_generator_v2.py
@@ -42,8 +42,8 @@ def pick_file_name():
                 print(f'  {i}  - "{test_file_names[i]}"')
             chosen_filepath = "../ascii_example_art/" + test_file_names[int(input("\nInput:\n"))] + ".txt"
         else:
-            print("\nOkay lets do this! please make sure the file is in the same directory as this '.py' file and input your '.txt' file name!")
-            custom_filepath = input("\nWhats your file name?\n")
+            print("\nOkay lets do this! so First:\nPlease do make sure the file is\n   - in the same directory as this '.py' file\n   - you dont mistype your '.txt' file name!\n\nWhats your file's name?")
+            custom_filepath = input("\nInput:\n")
             if ".txt" in custom_filepath:
                 chosen_filepath = custom_filepath
             else:
@@ -82,7 +82,7 @@ def file_open(filepath):
     # Designate Compatibility
     if file_compatibility == True:
         # Return the file Contents
-        return file_contents_string, image_width, image_height
+        return file_contents_string, image_width, (image_height * 2)
     
     else:
         print(f"I'm sorry it seems the file you picked is not compatible with this current system version.\nPlease make sure all rows are the same length as the top row which has a length of {image_width} characters.\n\nBe advised your file failed on line(s) {', '.join(non_compatible_rows)}")
@@ -95,7 +95,7 @@ def file_open(filepath):
 #   - Creates header string based on opened ascii file
 # =============================================================================
 
-def create_ppm_header(ascii_string, image_width, image_height):
+def create_ppm_header(image_width, image_height):
     
     # PPM type
     header_type = "P3"
@@ -103,7 +103,7 @@ def create_ppm_header(ascii_string, image_width, image_height):
     # Max RGB color Value
     color_max = 255
     
-    return f"{header_type}\n{image_width} {image_height}\n{color_max}"
+    return f"{header_type}\n{image_width} {image_height}\n{color_max}\n"
     
 
 # =============================================================================
@@ -155,7 +155,7 @@ def color_selections():
             if check_one == 'n':
                 user_color_selection = input("\nWhat color would you like to use today?\n\nInput:\n")
             else:
-                check_two = input(f"\nIm sorry '{user_color_selection}' is not a color we have listed, would you like to retry, input the RGB code directly or Give up? 1 or 2 or 3\n  (1) Retry\n  (2) Input RGB\n  (3) Give Up\n\nInput:\n")
+                check_two = input(f"\nIm sorry '{user_color_selection}' is not a color we have listed.\nWould you like to retry, input the RGB code directly or Give up?\n\nPlease Input 1 or 2 or 3\n  1  - Retry\n  2  - Input RGB\n  3  - Give Up\n\nInput:\n")
                 if check_two == "1":
                     user_color_selection = input("\nWhat color would you like to use today?\n\nInput:\n")
                 elif check_two == "2":
@@ -231,6 +231,43 @@ def ascii_color_swatch(color_selected):
 
 
 # =============================================================================
+# Function 6: PPM String Generator
+# 
+#   - Ascii art string contents --> PPM file content structure
+#       - Takes each ascii character and maps the corresponding RGB color code as a pixel
+# =============================================================================
+
+def ppm_file_contents(ascii_string, ascii_colors):
+    
+    # Define Output String
+    ppm_output_str = ""
+    
+    # P3 PPM Row Loop function
+    for row in ascii_string.splitlines():
+        for i in range(2):          # print each line twice to make up for text height
+            for pixel in row:
+                if pixel in ascii_colors.keys():
+                    ppm_output_str += f"{ascii_colors[pixel][0]} {ascii_colors[pixel][1]} {ascii_colors[pixel][2]} "
+            ppm_output_str += "\n"
+    
+    return ppm_output_str
+
+
+# =============================================================================
+# Function 7: File Naming & Output
+# =============================================================================
+
+def output_ppm(name, header, contents, image_height, image_width):
+    
+    # writing out a file using python I/O
+    with open(f"../image_outputs/{name}.ppm", "w") as ppm_file:
+        
+        ppm_file.write(header)
+        
+        ppm_file.write(contents)
+        print(f"\nAn image has been outputed to /image_outputs:\n\n  File Name: {name}.ppm\n  Image Height: {int(image_height/2)} pixels\n  Image Width: {image_width} pixels\n  Total Pixels: {(image_height/2) * image_width} pixels")
+
+# =============================================================================
 # Function Test Section (Temp)
 # 
 # **EDITS**
@@ -246,7 +283,7 @@ test_filepath = pick_file_name()
 if test_filepath != None:
     
     # Test print 1
-    print("\n" + test_filepath)
+    # print("\n" + test_filepath)
     
     # Try to find the file with a failsafe through try, except and else
     try:
@@ -255,7 +292,7 @@ if test_filepath != None:
         test_file_contents, ascii_width, ascii_height = file_open(test_filepath)
         
     except FileNotFoundError:
-        print("I'm Sorry, the file you requested was not found.\n\nPlease check the file directory again to make you didnt mistype the filename and that the file is in the right location.")
+        print("\n**Failed** I'm Sorry, the file you requested was not found.\n\nPlease check\n   - the file directory again to make you didnt mistype the filename\n   - the file is in the right location.\nThen run me again!")
     
     else:
         if test_file_contents != None:
@@ -264,22 +301,31 @@ if test_filepath != None:
             # print("\n" + test_file_contents)
     
             # Test Function 3 - Returns file PPM Header
-            ppm_header = create_ppm_header(test_file_contents, ascii_width, ascii_height)
+            ppm_header = create_ppm_header(ascii_width, ascii_height)
             
             # Test print 3
-            print(ppm_header)
+            # print(ppm_header)
             
             # Test Function 4 - Returns Selected color RGB list
             selected_rgb = color_selections()
             
             # Test print 4
-            print(selected_rgb)
+            # print(selected_rgb)
             
             if selected_rgb[0] != None:
                 
                 # Test Function 5 - Returns Color Swatch Diction Corresponding to Ascii Depth
                 color_swatch = ascii_color_swatch(selected_rgb)
                 
-                print(color_swatch)
-            
+                # Test print 5
+                # print(color_swatch)
+                
+                # Test Function 6  - Returns the ascii art file converted into ppm structured RGB codes that represent pixels
+                ppm_contents = ppm_file_contents(test_file_contents, color_swatch)
+                
+                # Test print 6
+                # print(ppm_contents)
+                
+                # Test Function 7 - Output ppm File 
+                output_ppm("test001", ppm_header, ppm_contents, ascii_height, ascii_width)
             
